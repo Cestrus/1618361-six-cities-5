df6f0d2e106b40a2b1088b27802aa0633c573cb5
Alexey Savchenko <wasxela@gmail.com>
2020-10-07 21:15:59 +0300

    have done some components, create initial routing

--- src/index.js
+++ src/index.js
@@ -2,44 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../src/components/app/app';
 
-
-const placeCardsData = [
-  {
-    imgURL: `img/apartment-01.jpg`,
-    price: 120,
-    name: `Beautiful & luxurious apartment at great location`,
-    type: `Apartment`,
-  },
-  {
-    imgURL: `img/room.jpg`,
-    price: 80,
-    name: `Wood and stone place`,
-    type: `Private room`,
-  },
-  {
-    imgURL: `img/apartment-02.jpg`,
-    price: 132,
-    name: `Canal View Prinsengracht`,
-    type: `Apartment`,
-  },
-  {
-    imgURL: `img/apartment-03.jpg`,
-    price: 180,
-    name: `Nice, cozy, warm big bed apartment`,
-    type: `Apartment`,
-  },
-  {
-    imgURL: `img/room.jpg`,
-    price: 80,
-    name: `Wood and stone place`,
-    type: `Private room`,
-  }
-];
-
-const cities = [`Paris`, `Cologne`, `Brussels`, `Amsterdam`, `Hamburg`, `Dusseldorf`];
-
-ReactDOM.render(
-    <App
-      placeCardsData = {placeCardsData}
-      cities = {cities}
-    />, document.querySelector(`#root`));
+ReactDOM.render(<App/>, document.querySelector(`#root`));
---
+++ src/mocks/galleryImgRoomMock.js
@@ -0,0 +1,3 @@
+const cities = [`room.jpg`, `apartment-01.jpg`, `apartment-02.jpg`, `apartment-03.jpg`, `studio-01.jpg`, `apartment-01.jpg`];
+
+export default cities;
